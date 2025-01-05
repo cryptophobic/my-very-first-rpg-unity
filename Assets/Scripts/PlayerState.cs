3fbc7588350c9091f132f4a9255b03c9211c9351
Dmytro <DmitryUglach@gmail.com>
2025-01-05 13:14:28 +0200

    Tile Palette WIP

---
+++ Assets/Scripts/PlayerState.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class PlayerState
+{
+    protected PlayerStateMachine stateMachine;
+    protected Player player;
+    protected Rigidbody2D rb;
+
+    protected float xInput;
+    protected float yInput;
+    private string animBoolName;
+
+    protected float stateTimer;
+    protected bool triggerCalled;
+
+    public PlayerState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName)
+    {
+        player = _player;
+        stateMachine = _stateMachine;
+        animBoolName = _animBoolName;
+    }
+
+    public virtual void Enter()
+    {
+        player.anim.SetBool(animBoolName, true);
+        rb = player.rb;
+        triggerCalled = false;
+    }
+
+    public virtual void Update()
+    {
+        stateTimer -= Time.deltaTime;
+        
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+        player.anim.SetFloat("yVelocity", rb.linearVelocity.y);
+    }
+
+    public virtual void Exit()
+    {
+        player.anim.SetBool(animBoolName, false);
+    }
+
+    public virtual void AnimationFinishTrigger()
+    {
+        triggerCalled = true;
+    }
+}
---
+++ Assets/Scripts/PlayerWallSlideState.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class PlayerWallSlideState : PlayerState
+{
+    public PlayerWallSlideState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            stateMachine.ChangeState(player.wallJumpState);
+            return;
+        }
+        
+        if (xInput != 0 && player.facingDir > 0 != xInput > 0)
+        {
+            stateMachine.ChangeState(player.idleState);
+        }
+
+        if (yInput < 0)
+        {
+            rb.linearVelocity = new Vector2(0f, rb.linearVelocity.y);
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2(0f, rb.linearVelocity.y * .7f);
+        }
+
+        if (player.IsGroundDetected())
+        {
+            stateMachine.ChangeState(player.idleState);
+        }
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+}
