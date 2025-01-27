399881f231a6a6aaa457e7d1438b69616f40e6f9
Dmytro <DmitryUglach@gmail.com>
2025-01-27 19:03:15 +0200

    Test

--- Assets/EnemySkeletonAnimationTriggers.cs
+++ Assets/EnemySkeletonAnimationTriggers.cs
@@ -4,6 +4,7 @@ public class EnemySkeletonAnimationTriggers : MonoBehaviour
 {
     private EnemySkeleton enemy => GetComponentInParent<EnemySkeleton>();
 
+    
     private void AnimationTrigger()
     {
         enemy.AnimationFinishTrigger();
